62c1d863b584ae516230c4f7394a679d3bd27e79
Trevor Seitz <trevinla@gmail.com>
2018-06-25 14:28:25 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,8 +10,10 @@ class StudentsController < ApplicationController
   end
 
   def activate
-    binding.pry
-      @student.active = !@student.active
+    @student = Student.find(params[:id])
+    @student.active = !@student.active
+    @student.save
+    redirect_to student_path(@student)
   end
 
   private
