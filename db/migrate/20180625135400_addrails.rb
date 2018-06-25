c3bce974138a4297d80b4e22e7a929fa35f3c560
Trevor Seitz <trevinla@gmail.com>
2018-06-25 13:58:05 +0000

    automatically backed up by learn

--- db/migrate/20180625135400_addrails.rb
+++
@@ -1,4 +0,0 @@
-class Addrails < ActiveRecord::Migration
-  def change
-  end
-end
--- db/migrate/20180625135516_add_column_to_students.rb
+++ db/migrate/20180625135516_add_column_to_students.rb
@@ -1,4 +1,5 @@
 class AddColumnToStudents < ActiveRecord::Migration
   def change
+    ALTER TABLE students ADD COLUMN "active" BOOLEAN DEFAULT FALSE;
   end
 end
